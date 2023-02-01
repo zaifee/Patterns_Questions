5e24256281fef5795408222d8162e5586c5111bf
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-01-31 20:48:47 -0800

    Create Pattern1.cpp

---
+++ Pattern1.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*  Printing this Pattern
+    
+              1 2 3 4 
+              1 2 3 4
+              1 2 3 4
+              1 2 3 4
+              
+    */
+    
+    int n;
+    cin>>n;
+    int i = 1;
+    while(i <= n ){
+        int j = 1;
+        while(j <= n){
+            cout<<j;
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+    }
+    
+    
+    
+    
+   
+    return 0;
+}
